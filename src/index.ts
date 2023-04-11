bfd080068318b7eec7e3be3a767812769ae70cf5
Josh Goldberg <git@joshuakgoldberg.com>
2023-04-11 12:04:29 -0400

    docs: add JSDoc descriptions

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,21 @@ const defaultAliases: Record<string, string[]> = {
 
 const defaultUselessWords = ["a", "an", "i", "in", "of", "s", "the"];
 
+/**
+ * @param docs - Any amount of docs text, such as from a JSDoc description.
+ * @param name - Name of the entity the docs text is describing.
+ * @param options - Additional options to customize informativity checking.
+ * @returns Whether the docs include at least one word with new information.
+ *
+ * @example
+ * ```js
+ * areDocsInformative("The user id.", "userId"); // false
+ * ```
+ * @example
+ * ```js
+ * areDocsInformative("Retrieved user id.", "userId"); // true
+ * ```
+ */
 export function areDocsInformative(
 	docs: string | string[],
 	name: string | string[],
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,40 @@
 export interface InformativeDocsOptions {
+	/**
+	 * Words that can be considered synonyms (aliases) of each other.
+	 *
+	 * @default
+	 * ```json
+	 * {
+	 *   "a": ["an", "our"]
+	 * }
+	 * ```
+	 *
+	 * @example
+	 * With `{ aliases: { emoji: ["smiley", "winkey"] } }`,
+	 * the following comment would be considered uninformative:
+	 * ```js
+	 * /** Default smiley/winkey. *\/
+	 * export const defaultSmiley = "🙂";
+	 * ```
+	 */
 	aliases?: Record<string, string[]>;
+
+	/**
+	 * Words that are ignored when searching for one that adds meaning.
+	 *
+	 * @default
+	 * ```json
+	 * ["a", "an", "i", "in", "of", "s", "the"]
+	 * ```
+	 *
+	 * @example
+	 * With `{ uselessWords: ["our"] }`, the following comment would
+	 * be considered uninformative:
+	 * ```js
+	 * /** Our text. *\/
+	 * export const text = ":)";
+	 * ```
+	 */
 	existingName?: string | string[];
 	uselessWords?: string[];
 }
